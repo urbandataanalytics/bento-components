23ad365ec3551390be7217c41376311779df7bf7
Oscar Bustos <oscarbustosgarrido@gmail.com>
2020-06-12 12:47:30 +0200

    feat: update avatar fonts

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -26,7 +26,8 @@ const componentSizes = theme => ({
 });
 
 const StyledAvatar = styled.div`
-  ${props => componentSizes(props.theme)[props.size]};
+  ${({ theme, size }) => componentSizes(theme)[size]};
+  ${({ theme }) => theme.texts.p2b}
   border-radius: ${({ theme }) => theme.components.avatarBorderRadius};
   background-color: ${({ theme, customColor }) =>
     customColor || theme.components.avatarBackgroundColor};
