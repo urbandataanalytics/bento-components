7c6eacfec2a774308534b7fa1e65d822723b04c8
Oscar Bustos <oscarbustosgarrido@gmail.com>
2020-06-19 10:19:06 +0200

    fix: card header gap

--- src/components/Card/CardHeader/index.js
+++ src/components/Card/CardHeader/index.js
@@ -10,7 +10,6 @@ const StyledCardHeader = styled.header`
 const StyledContainer = styled.div`
   display: grid;
   grid-template-columns: 55px 1fr 55px;
-  grid-column-gap: 11px;
   align-items: top;
   border-bottom: 1px solid ${({ theme }) => theme.color.charcoal400};
   padding-bottom: ${({ theme }) => theme.spacings.small3};
