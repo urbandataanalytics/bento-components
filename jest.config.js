97efbceb2db4df0788548181ecf1a3be605313ac
Miguel Ángel Gutiérrez Moreno <magutierrezmoreno@gmail.com>
2020-09-24 12:27:54 +0200

    Feature/codeclimate integration (#97)
    * feat: codeclimated integration to circleCI

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  coverageReporters: ['text'],
+  coverageReporters: ['lcov'],
   moduleFileExtensions: ['js', 'json'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
