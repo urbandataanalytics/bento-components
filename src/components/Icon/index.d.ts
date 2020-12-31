e956471f139b33fa8d9c17ec44f7a0dd7c1bb23e
Oscar Bustos <oscarbustosgarrido@gmail.com>
2020-12-31 09:02:55 +0100

    feat: admit extralarge on icons

--- src/components/Icon/index.d.ts
+++ src/components/Icon/index.d.ts
@@ -1,6 +1,6 @@
 export interface Props {
   color?: 'primary' | 'secondary' | 'tertiary' | 'info' | 'success' | 'warning' | 'critical';
-  size?: 'small' | 'medium' | 'large';
+  size?: 'small' | 'medium' | 'large' | 'extralarge';
   customColor?: string;
   viewBox?: string;
   children: React.ReactNode;
