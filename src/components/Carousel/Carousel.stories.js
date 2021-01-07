20ea505e428b2e125c9aa4328ed13248c3b31ba3
Nany Thery <nadinethery@gmail.com>
2021-01-07 18:23:41 +0100

    feat: carousel test update

--- src/components/Carousel/Carousel.stories.js
+++ src/components/Carousel/Carousel.stories.js
@@ -42,3 +42,34 @@ export const Playground = () => {
     </div>
   );
 };
+
+export const withThumbnailsEnabled = () => {
+  const slidesImages = [
+    'https://images.adsttc.com/media/images/59a7/3256/b22e/3828/7b00/0277/slideshow/4.jpg?1504129602',
+    'https://images.adsttc.com/media/images/59a7/3400/b22e/3828/7b00/0280/slideshow/21.jpg?1504130037',
+    'https://images.adsttc.com/media/images/59a7/33f3/b22e/38a3/0300/04a3/slideshow/20.jpg?1504130022',
+    'https://images.adsttc.com/media/images/59a7/3495/b22e/3828/7b00/0283/slideshow/30.jpg?1504130176',
+    'https://images.adsttc.com/media/images/59a7/3466/b22e/3828/7b00/0282/slideshow/28.jpg?1504130127'
+  ];
+
+  const containerStyle = {
+    padding: '2rem',
+    width: '50%',
+    height: '250px'
+  };
+  return (
+    <div style={containerStyle}>
+      <Carousel
+        slides={slidesImages}
+        draggable={boolean('Draggable', false)}
+        controlOffset={text('Control Offset', '10px')}
+        loop={boolean('Loop', false)}
+        rounded={boolean('Rounded', true)}
+        startIndex={number('Start Index', 0)}
+        thumbnailsEnabled={boolean('Thumbnails Enabled', true)}
+        thumbnailStartIndex={number('Thumbnails Start Index', 0)}
+        thumbCount={number('Thumbnail count', 3)}
+      ></Carousel>
+    </div>
+  );
+};
