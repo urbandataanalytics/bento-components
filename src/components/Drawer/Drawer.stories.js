f148b72487f71e86cec87dd007a08272bb8460e9
Nany Thery <nadinethery@gmail.com>
2021-01-12 16:48:06 +0100

    feat: drawer controls update

--- src/components/Drawer/Drawer.stories.js
+++ src/components/Drawer/Drawer.stories.js
@@ -5,45 +5,105 @@ import ListItem from '../List/ListItem';
 import IconFolder from '../../icons/Folder';
 import List from '../List';
 import { IconAsset, IconEye, IconFilter, IconNavigation, IconUser } from '../../icons';
-import { select, text } from '@storybook/addon-knobs';
-import { action } from '@storybook/addon-actions';
 import Accordion from '../AccordionList/Accordion';
 import { ButtonLink, Grid } from '../../index';
 
 export default {
   title: 'Drawer',
-  component: Drawer
+  component: Drawer,
+  subcomponents: { Accordion, ButtonLink, Grid, List, ListItem },
+  argTypes: {
+    children: {
+      description: ' The content of the Drawer.                                           ',
+      control: 'none',
+      table: {
+        category: 'content'
+      }
+    },
+    header: {
+      description: "Title of the Drawer that will be rendered in the Drawer's header.",
+      control: 'text',
+      table: {
+        category: 'content'
+      }
+    },
+    offsetBottom: {
+      description: 'The bottom offset of the Drawer.',
+      table: {
+        category: 'format'
+      }
+    },
+    offsetLeft: {
+      description: 'The Left offset of the Drawer.',
+      table: {
+        category: 'format'
+      }
+    },
+    offsetRight: {
+      description: 'The right offset of the Drawer.',
+      table: {
+        category: 'format'
+      }
+    },
+    offsetTop: {
+      description: 'The top offset of the Drawer.',
+      table: {
+        category: 'format'
+      }
+    },
+    onClose: {
+      description: ' Function for handling onClose event.  ',
+      action: 'onCloseEvent',
+      table: {
+        category: 'Events'
+      }
+    },
+    open: {
+      description: 'Shows the drawer',
+      table: {
+        category: 'behaviour'
+      }
+    },
+    position: {
+      description: 'The position on which side the Drawer should appear.',
+      table: {
+        category: 'format'
+      }
+    },
+    showOverlay: {
+      description: 'If `true`, the Drawer will have cloudy background.',
+      table: {
+        category: 'format'
+      }
+    },
+    subHeader: {
+      description: "Subtitle of the Drawer that will be rendered in the Drawer's header.",
+      table: {
+        category: 'content'
+      }
+    },
+    width: {
+      description: 'Width of the drawer',
+      table: {
+        category: 'format'
+      }
+    }
+  },
+  args: {
+    open: true,
+    position: 'left',
+    width: '320px',
+    offsetTop: '0px',
+    offsetRight: '0px',
+    offsetLeft: '0px',
+    offsetBottom: '0px'
+  }
 };
 
-const getCommonProps = () => {
-  return {
-    position: select('Position', ['right', 'left'], 'right'),
-    width: text('Width', '320px'),
-    offsetTop: text('Offset Top', '0px'),
-    offsetRight: text('Offset Right', '0px'),
-    offsetLeft: text('Offset Left', '0px'),
-    offsetBottom: text('Offset Bottom', '0px')
-  };
-};
-
-export const Playground = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClose = () => {
-    setIsOpen(false);
-    action('onClose')();
-  };
-
+export const Playground = args => {
   return (
     <>
-      <Button onClick={() => setIsOpen(!isOpen)}>{isOpen ? 'Close' : 'Open'} drawer</Button>
-
-      <Drawer
-        {...getCommonProps()}
-        header={<h4>{text('Title', 'Title')}</h4>}
-        subHeader={<small>{text('Subtitle', 'Subtitle')}</small>}
-        open={isOpen}
-        onClose={handleClose}
-      >
+      <Drawer {...args}>
         <Accordion header={'Accordion title 1'} leftContent={<IconNavigation />}>
           <List>
             <ListItem leftContent={<IconUser />}>Text</ListItem>
@@ -51,7 +111,6 @@ export const Playground = () => {
             <ListItem leftContent={<IconFolder />}>Text</ListItem>
           </List>
         </Accordion>
-
         <Accordion header={'Accordion title 2'} leftContent={<IconUser />}>
           <Grid columns={'90% 10%'} gap={'5px'}>
             <List>
@@ -105,4 +164,81 @@ export const Playground = () => {
       </Drawer>
     </>
   );
+};
+
+Playground.args = {
+  header: 'Accordion title 1',
+  subHeader: 'Subtitle'
+};
+
+export const RightPosition = args => {
+  return (
+    <>
+      <Drawer {...args}>
+        <Accordion header={'Accordion title 1'} leftContent={<IconNavigation />}>
+          <List>
+            <ListItem leftContent={<IconUser />}>Text</ListItem>
+            <ListItem leftContent={<IconAsset />}>Text</ListItem>
+            <ListItem leftContent={<IconFolder />}>Text</ListItem>
+          </List>
+        </Accordion>
+        <Accordion header={'Accordion title 2'} leftContent={<IconUser />}>
+          <Grid columns={'90% 10%'} gap={'5px'}>
+            <List>
+              <ListItem rightContent={<IconFilter size={'small'} />}>Text 1</ListItem>
+              <ListItem rightContent={<IconFilter size={'small'} />}>Text 2</ListItem>
+              <ListItem rightContent={<IconFilter size={'small'} />}>Text 3</ListItem>
+            </List>
+            <div>
+              <div
+                style={{
+                  marginBottom: '4px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  alignContent: 'center',
+                  justifyContent: 'center'
+                }}
+              >
+                <ButtonLink size={'small'} variant={'primary'} style={{ minHeight: '34px' }}>
+                  <IconEye />
+                </ButtonLink>
+              </div>
+              <div
+                style={{
+                  marginBottom: '4px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  alignContent: 'center',
+                  justifyContent: 'center'
+                }}
+              >
+                <ButtonLink size={'small'} variant={'secondary'} style={{ minHeight: '34px' }}>
+                  <IconEye />
+                </ButtonLink>
+              </div>
+              <div
+                style={{
+                  marginBottom: '4px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  alignContent: 'center',
+                  justifyContent: 'center'
+                }}
+              >
+                <ButtonLink size={'small'} variant={'secondary'} style={{ minHeight: '34px' }}>
+                  <IconEye />
+                </ButtonLink>
+              </div>
+            </div>
+          </Grid>
+        </Accordion>
+      </Drawer>
+    </>
+  );
+};
+
+RightPosition.args = {
+  header: 'Accordion title 1',
+  subHeader: 'Subtitle',
+  position: 'right'
 };
