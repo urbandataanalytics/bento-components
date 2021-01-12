83a64ef97bedf8659d18d033970e928abc430c3b
Nany Thery <nadinethery@gmail.com>
2021-01-12 15:07:12 +0100

    feat: carousel controls update

--- src/components/Carousel/Carousel.stories.js
+++ src/components/Carousel/Carousel.stories.js
@@ -1,75 +1,142 @@
 import React from 'react';
 import Carousel from './index';
-import { action } from '@storybook/addon-actions';
-import { number, text, boolean } from '@storybook/addon-knobs';
-import Button from './index';
-import { string } from 'prop-types';
-import { ActionIcon } from '../../icons';
-import { IconChevronLeft } from '../../icons';
 
 export default {
   title: 'Carousel',
-  component: Carousel
+  component: Carousel,
+  argTypes: {
+    controlOffset: {
+      description:
+        'Defines the separation of the `next` and `previous` controls from the edge of the image',
+      table: {
+        category: 'format'
+      }
+    },
+    draggable: {
+      description: 'Allows to drag the image and slide to the sides without click the buttons',
+      table: {
+        category: 'behaviour'
+      }
+    },
+    loop: {
+      description:
+        'Makes the carousel to start over again from the first slide when it arrives to the last one',
+      table: {
+        category: 'behaviour'
+      }
+    },
+    nextButton: {
+      description: 'Allows to define a custom `next` button ',
+      control: 'none',
+      table: {
+        category: 'behaviour'
+      }
+    },
+    onChange: {
+      description: 'Handles behaviour lauched with event',
+      action: 'onChange',
+      table: {
+        category: 'events'
+      }
+    },
+    onClick: {
+      description: '',
+      table: {
+        category: 'events'
+      }
+    },
+    onThumbClick: {
+      description: ' Handles behaviour lauched with event',
+      action: 'clickedOnThumbnail',
+      table: {
+        category: 'events'
+      }
+    },
+    prevButton: {
+      description: ' Allows to define a custom `previous` button                   ',
+      table: {
+        category: 'events'
+      }
+    },
+    rounded: {
+      description: ' Applies rounded corners',
+      table: {
+        category: 'format'
+      }
+    },
+    slides: {
+      description: 'Array of images used by the Carousel',
+      table: {
+        category: 'content'
+      }
+    },
+    startIndex: {
+      description: 'Sets the starting image to the position of the array `slides`defined',
+      table: {
+        category: 'behaviour'
+      }
+    },
+    thumbCount: {
+      description: 'Quantity of thumbnail images to be shown next to the main image',
+      table: {
+        category: 'behaviour'
+      }
+    },
+    thumbnailStartIndex: {
+      description: 'Sets the starting image for the thumbnails according to he ',
+      table: {
+        category: 'behaviour'
+      }
+    },
+    thumbnailsEnabled: {
+      description:
+        'Enables the thumbnail preview of the images of the `slideds`array next to the carousel ',
+      table: {
+        category: 'behaviour'
+      }
+    }
+  },
+  args: {
+    slides: [
+      'https://images.adsttc.com/media/images/59a7/3256/b22e/3828/7b00/0277/slideshow/4.jpg?1504129602',
+      'https://images.adsttc.com/media/images/59a7/3400/b22e/3828/7b00/0280/slideshow/21.jpg?1504130037',
+      'https://images.adsttc.com/media/images/59a7/33f3/b22e/38a3/0300/04a3/slideshow/20.jpg?1504130022',
+      'https://images.adsttc.com/media/images/59a7/3495/b22e/3828/7b00/0283/slideshow/30.jpg?1504130176',
+      'https://images.adsttc.com/media/images/59a7/3466/b22e/3828/7b00/0282/slideshow/28.jpg?1504130127'
+    ],
+    draggable: false,
+    controlOffset: '10px',
+    loop: false,
+    rounded: false,
+    startIndex: 0,
+    thumbnailsEnabled: false,
+    thumbnailStartIndex: 0,
+    thumbCount: 3
+  }
 };
 
-export const Playground = () => {
-  const slidesImages = [
-    'https://images.adsttc.com/media/images/59a7/3256/b22e/3828/7b00/0277/slideshow/4.jpg?1504129602',
-    'https://images.adsttc.com/media/images/59a7/3400/b22e/3828/7b00/0280/slideshow/21.jpg?1504130037',
-    'https://images.adsttc.com/media/images/59a7/33f3/b22e/38a3/0300/04a3/slideshow/20.jpg?1504130022',
-    'https://images.adsttc.com/media/images/59a7/3495/b22e/3828/7b00/0283/slideshow/30.jpg?1504130176',
-    'https://images.adsttc.com/media/images/59a7/3466/b22e/3828/7b00/0282/slideshow/28.jpg?1504130127'
-  ];
+const containerStyle = {
+  padding: '2rem',
+  width: '50%',
+  height: '250px'
+};
 
-  const containerStyle = {
-    padding: '2rem',
-    width: '50%',
-    height: '250px'
-  };
+export const Playground = args => {
   return (
     <div style={containerStyle}>
-      <Carousel
-        slides={slidesImages}
-        draggable={boolean('Draggable', false)}
-        controlOffset={text('Control Offset', '10px')}
-        loop={boolean('Loop', false)}
-        rounded={boolean('Rounded', false)}
-        startIndex={number('Start Index', 0)}
-        thumbnailsEnabled={boolean('Thumbnails Enabled', false)}
-        thumbnailStartIndex={number('Thumbnails Start Index', 0)}
-        thumbCount={number('Thumbnail count', 3)}
-      ></Carousel>
+      <Carousel {...args}></Carousel>
     </div>
   );
 };
 
-export const withThumbnailsEnabled = () => {
-  const slidesImages = [
-    'https://images.adsttc.com/media/images/59a7/3256/b22e/3828/7b00/0277/slideshow/4.jpg?1504129602',
-    'https://images.adsttc.com/media/images/59a7/3400/b22e/3828/7b00/0280/slideshow/21.jpg?1504130037',
-    'https://images.adsttc.com/media/images/59a7/33f3/b22e/38a3/0300/04a3/slideshow/20.jpg?1504130022',
-    'https://images.adsttc.com/media/images/59a7/3495/b22e/3828/7b00/0283/slideshow/30.jpg?1504130176',
-    'https://images.adsttc.com/media/images/59a7/3466/b22e/3828/7b00/0282/slideshow/28.jpg?1504130127'
-  ];
-
-  const containerStyle = {
-    padding: '2rem',
-    width: '50%',
-    height: '250px'
-  };
+export const withThumbnailsEnabled = args => {
   return (
     <div style={containerStyle}>
-      <Carousel
-        slides={slidesImages}
-        draggable={boolean('Draggable', false)}
-        controlOffset={text('Control Offset', '10px')}
-        loop={boolean('Loop', false)}
-        rounded={boolean('Rounded', true)}
-        startIndex={number('Start Index', 0)}
-        thumbnailsEnabled={boolean('Thumbnails Enabled', true)}
-        thumbnailStartIndex={number('Thumbnails Start Index', 0)}
-        thumbCount={number('Thumbnail count', 3)}
-      ></Carousel>
+      <Carousel {...args}></Carousel>
     </div>
   );
+};
+
+withThumbnailsEnabled.args = {
+  thumbnailsEnabled: true
 };
