d9a283c4dc55c82a375d01421f0ea6f19fb2a4c6
Nany Thery <nadinethery@gmail.com>
2021-01-14 11:09:16 +0100

    feat: textlink controls

--- src/components/TextLink/TextLink.stories.js
+++ src/components/TextLink/TextLink.stories.js
@@ -1,29 +1,78 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { select, text, boolean } from '@storybook/addon-knobs';
 import TextLink from './index';
 
 export default {
   title: 'TextLink',
-  component: TextLink
+  component: TextLink,
+  argTypes: {
+    children: {
+      description: 'Elements within the link',
+      control: 'text',
+      table: { category: 'content' }
+    },
+    className: {
+      description: 'Adds class to the element',
+      control: 'none',
+      table: {
+        category: 'others'
+      }
+    },
+    disabled: {
+      description: 'Disables the link',
+      table: {
+        category: 'behaviour'
+      }
+    },
+    external: {
+      description:
+        'Sets the options `target= "__blank"` and the attributes `noopener`and `noreferrer to the`rel` attribute',
+      table: {
+        category: 'behaviour'
+      }
+    },
+    href: {
+      description: 'The url where it must link to',
+      table: {
+        category: 'content'
+      }
+    },
+    size: {
+      description: 'Sets the font size of the link',
+      table: {
+        category: 'format'
+      }
+    },
+    tabIndex: {
+      description: 'Specifies the tab order of an element.',
+      control: 'none',
+      table: {
+        category: 'others'
+      }
+    },
+    variant: {
+      description: 'Sets different styles according to the selected variant',
+      table: {
+        category: 'format'
+      }
+    }
+  },
+  args: {
+    children: 'Link Text',
+    disabled: false,
+    external: false,
+    href: '#',
+    size: 'medium',
+    variant: 'primary'
+  }
 };
 
-export const Playground = () => {
+export const Playground = args => {
   const containerStyle = {
     padding: '2rem'
   };
   return (
     <div style={containerStyle}>
-      <TextLink
-        disabled={boolean('Disabled', false)}
-        external={boolean('External', false)}
-        href={text('Link', '#')}
-        size={select('Sizes', ['medium', 'large'], 'medium')}
-        variant={select('Variant', ['primary', 'secondary'], 'primary')}
-        onClick={action('clicked')}
-      >
-        {text('Link Text', 'Example Text')}
-      </TextLink>
+      <TextLink {...args}></TextLink>
     </div>
   );
 };
