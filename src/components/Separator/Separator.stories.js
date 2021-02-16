54e5144cb0bc54fd46203aa873da0bba7b8cb4a5
Nany Thery <nadinethery@gmail.com>
2021-02-16 16:30:00 +0100

    feat: new component

---
+++ src/components/Separator/Separator.stories.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Separator from './index';
+
+export default {
+  title: 'Separator',
+  component: Separator,
+  argTypes: {
+    label: {
+      description: 'Accepts a string to shown in the middle of the separator',
+      table: {
+        category: 'content'
+      }
+    }
+  }
+};
+
+export const Playground = args => {
+  const containerStyle = {
+    padding: '2rem',
+    width: '500px'
+  };
+
+  return (
+    <div style={containerStyle}>
+      <Separator {...args} />
+    </div>
+  );
+};
+
+export const WithLabel = args => {
+  const containerStyle = {
+    padding: '2rem',
+    width: '500px'
+  };
+  return (
+    <div style={containerStyle}>
+      <Separator {...args} />
+    </div>
+  );
+};
+
+WithLabel.args = {
+  label: 'O'
+};
